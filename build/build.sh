b7782478e367f3b7b67353bbc05ced2240cbd303
Moideen Parampil Mohamed Reyas <mreyas@us.ibm.com>
2020-02-10 20:30:43 -0600

    fix build script

--- build/build.sh
+++ build/build.sh
@@ -6,17 +6,17 @@ cd /opt/ssfs/runtime/docker-samples/imagebuild
 cp generateImages.sh generateImages.sh.bkp
 cp generateImages.sh.in generateImages.sh
 ./generateImages.sh --MODE=app --EXPORT=true --WAR_FILES=smcfs;sbc
-./generateImages.sh --MODE=agent --EXPORT=true
+#./generateImages.sh --MODE=agent --EXPORT=true
 echo PUSH_DOCKERCFG_PATH $PUSH_DOCKERCFG_PATH
 echo "Content of the file $(cat $PUSH_DOCKERCFG_PATH/.dockercfg)"
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
 echo "OUTPUT_REGISTRY" ${OUTPUT_REGISTRY}
 echo "RELEASE_VERSION" ${RELEASE_VERSION}
-buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
+#buildah tag om-agent:10.0 ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
 buildah tag om-app:10.0 ${OUTPUT_REGISTRY}/${OMS_APP_IMG_STREAM}:${RELEASE_VERSION}
 echo "buildah tag done"
 sudo docker images
-buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
+#buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OMS_AGENT_IMG_STREAM}:${RELEASE_VERSION}
 buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OMS_APP_IMG_STREAM}:${RELEASE_VERSION}
 echo "buildah push done"
 
